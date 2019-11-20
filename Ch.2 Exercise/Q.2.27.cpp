b0d1fb400776cb2504d26e856c6e0660f6d1057d
Muhammad Zeeshan <56844797+Zee604@users.noreply.github.com>
2019-11-20 13:56:43 +0500

    Add files via upload

---
+++ Ch.2 Exercise/Q.2.27.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char symbol;
+	cout<<"Enter any Integer";
+	cin>>symbol;
+	cout<<static_cast<int>(symbol);
+	return 0;
+}
---
+++ Ch.2 Exercise/Q.2.29.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    a=0;
+	cout<<"\nSide of Square(cm)"<<"\tPerimeter of Square(cm)"<<"\t\tArea of Square(cm)"<<endl;
+	cout<<"  "<<a<<"\t\t\t\t"<<a*a<<"\t\t\t\t"<<a*a*a<<endl;
+	a=a+1;
+	cout<<"  "<<a<<"\t\t\t\t"<<a*a<<"\t\t\t\t"<<a*a*a<<endl;
+	a=a+1;
+	cout<<"  "<<a<<"\t\t\t\t"<<a*a<<"\t\t\t\t"<<a*a*a<<endl;
+	a=a+1;
+	cout<<"  "<<a<<"\t\t\t\t"<<a*a<<"\t\t\t\t"<<a*a*a<<endl;
+	a=a+1;
+	cout<<"  "<<a<<"\t\t\t\t"<<a*a<<"\t\t\t\t"<<a*a*a<<endl;
+}
