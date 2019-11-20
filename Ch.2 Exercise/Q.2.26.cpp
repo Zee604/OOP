47cd9e6e8089b041345308d6ad16cf7b319bed47
Muhammad Zeeshan <56844797+Zee604@users.noreply.github.com>
2019-11-20 13:06:13 +0500

    Add files via upload

---
+++ Ch.2 Exercise/Q.2.26.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"\n\n\t* * * * * * * *"<<endl;
+    cout<<" \t * * * * * * * *"<<endl;	
+    	cout<<"\t* * * * * * * *"<<endl;
+    cout<<" \t * * * * * * * *"<<endl;	
+    	cout<<"\t* * * * * * * *"<<endl;
+    cout<<" \t * * * * * * * *"<<endl;	
+    	cout<<"\t* * * * * * * *"<<endl;
+    cout<<" \t * * * * * * * *"<<endl;	
+}
