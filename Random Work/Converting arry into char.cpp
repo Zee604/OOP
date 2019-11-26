19de6ac54771768fe2207a6ef88043eb3d1f9d4f
Muhammad Zeeshan <56844797+Zee604@users.noreply.github.com>
2019-11-26 22:52:01 +0500

    Add files via upload

---
+++ Random Work/Converting arry into char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+// Header file
+using namespace std;
+main()
+{ string arr;
+cout<< "Enter Value ";
+cin>> arr;
+int feet, inche;
+
+feet =arr[0];
+inche = (arr[2]);
+cout<< "Feet is equal to " << (char)feet<<"\n";
+cout<< "Inches is equal to " <<(char)inche<<"\n";
+};
